d58f40d7ed84daecc88ae8dad91bc21785a51da4
Naol Dame <naododa5566@gmail.com>
2022-05-27 11:48:57 +0300

    onpress handler

--- my-recipe/src/screens/Categories/CategoriesScreen.js
+++ my-recipe/src/screens/Categories/CategoriesScreen.js
@@ -26,3 +26,25 @@ export default function CategoriesScreen(props) {
       headerRight: () => <View />,
     });
   }, []);
+  const onPressCategory = (item) => {
+    const title = item.name;
+    const category = item;
+    navigation.navigate("RecipesList", { category, title });
+  };
+
+  const renderCategory = ({ item }) => (
+    <TouchableHighlight underlayColor="rgba(73,182,77,0.9)" onPress={() => onPressCategory(item)}>
+      <View style={styles.categoriesItemContainer}>
+        <Image style={styles.categoriesPhoto} source={{ uri: item.photo_url }} />
+        <Text style={styles.categoriesName}>{item.name}</Text>
+        <Text style={styles.categoriesInfo}>{getNumberOfRecipes(item.id)} recipes</Text>
+      </View>
+    </TouchableHighlight>
+  );
+
+  return (
+    <View>
+      <FlatList data={categories} renderItem={renderCategory} keyExtractor={(item) => `${item.id}`} />
+    </View>
+  );
+}
