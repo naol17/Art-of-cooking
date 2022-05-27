ea9fa04d54479991105c2e039d0b488000322919
Naol Dame <naododa5566@gmail.com>
2022-05-27 12:23:09 +0300

    Click Handlers

--- my-recipe/src/screens/Recipe/Recipe.js
+++ my-recipe/src/screens/Recipe/Recipe.js
@@ -7,3 +7,42 @@ import BackButton from "../../components/BackButton/BackButton";
 import ViewIngredientsButton from "../../components/ViewIngredientsButton/ViewIngredientsButton";
 
 const { width: viewportWidth } = Dimensions.get("window");
+
+export default function RecipeScreen(props) {
+    const { navigation, route } = props;
+  
+    const item = route.params?.item;
+    const category = getCategoryById(item.categoryId);
+    const title = getCategoryName(category.id);
+  
+    const [activeSlide, setActiveSlide] = useState(0);
+  
+    const slider1Ref = useRef();
+  
+    useLayoutEffect(() => {
+      navigation.setOptions({
+        headerTransparent: "true",
+        headerLeft: () => (
+          <BackButton
+            onPress={() => {
+              navigation.goBack();
+            }}
+          />
+        ),
+        headerRight: () => <View />,
+      });
+    }, []);
+    const renderImage = ({ item }) => (
+        <TouchableHighlight>
+          <View style={styles.imageContainer}>
+            <Image style={styles.image} source={{ uri: item }} />
+          </View>
+        </TouchableHighlight>
+      );
+    
+      const onPressIngredient = (item) => {
+        var name = getIngredientName(item);
+        let ingredient = item;
+        navigation.navigate("Ingredient", { ingredient, name });
+      };
+      
