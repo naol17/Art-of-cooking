6b9aca5dac279c6f14d744ad7f5813c506b5f53f
Naol Dame <naododa5566@gmail.com>
2022-05-27 12:17:19 +0300

    update style.js

--- my-recipe/src/screens/IngredientsDetail/style.js
+++ my-recipe/src/screens/IngredientsDetail/style.js
@@ -9,3 +9,28 @@ const numColumns = 3;
 const RECIPE_ITEM_HEIGHT = 100;
 const RECIPE_ITEM_OFFSET = 10;
 const RECIPE_ITEM_MARGIN = RECIPE_ITEM_OFFSET * 2;
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      margin: RECIPE_ITEM_OFFSET,
+      marginTop: 30,
+      width: (SCREEN_WIDTH - RECIPE_ITEM_MARGIN) / numColumns - RECIPE_ITEM_OFFSET,
+      height: RECIPE_ITEM_HEIGHT + 60
+    },
+    title: {
+      margin: 10,
+      marginBottom: 5,
+      color: 'black',
+      fontSize: 13,
+      textAlign: 'center'
+    },
+    photo: {
+      width: (SCREEN_WIDTH - RECIPE_ITEM_MARGIN) / numColumns - RECIPE_ITEM_OFFSET,
+      height: RECIPE_ITEM_HEIGHT,
+      borderRadius: 60
+    }
+  });
+  
+  export default styles;
