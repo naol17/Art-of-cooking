80a294c201b3775a529da6ac15038d9b7b28c386
Naol Dame <naododa5566@gmail.com>
2022-05-27 12:05:51 +0300

    ingredients page

---
+++ my-recipe/src/screens/Ingredient/IngredientScreen.js
@@ -0,0 +1,21 @@
+import React, { useLayoutEffect } from "react";
+import { FlatList, ScrollView, Text, View, Image, TouchableHighlight } from "react-native";
+import styles from "./styles";
+import { getIngredientUrl, getRecipesByIngredient, getCategoryName } from "../../data/MockDataAPI";
+
+export default function IngredientScreen(props) {
+  const { navigation, route } = props;
+
+  const ingredientId = route.params?.ingredient;
+  const ingredientUrl = getIngredientUrl(ingredientId);
+  const ingredientName = route.params?.name;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: route.params?.name,
+    });
+  }, []);
+
+  const onPressRecipe = (item) => {
+    navigation.navigate("Recipe", { item });
+  };
