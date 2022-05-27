721ecdf56116661f10a3bd81f66deb5deb6e445e
Naol Dame <naododa5566@gmail.com>
2022-05-27 12:13:04 +0300

    Update Ingredients Pages

--- my-recipe/src/screens/IngredientsDetail/IngredientsDetailScreen.js
+++ my-recipe/src/screens/IngredientsDetail/IngredientsDetailScreen.js
@@ -23,3 +23,19 @@ export default function IngredientsDetailsScreen(props) {
     let ingredient = item.ingredientId;
     navigation.navigate("Ingredient", { ingredient, name });
   };
+  const renderIngredient = ({ item }) => (
+    <TouchableHighlight underlayColor="rgba(73,182,77,0.9)" onPress={() => onPressIngredient(item[0])}>
+      <View style={styles.container}>
+        <Image style={styles.photo} source={{ uri: item[0].photo_url }} />
+        <Text style={styles.title}>{item[0].name}</Text>
+        <Text style={{ color: "grey" }}>{item[1]}</Text>
+      </View>
+    </TouchableHighlight>
+  );
+
+  return (
+    <View>
+      <FlatList vertical showsVerticalScrollIndicator={false} numColumns={3} data={ingredientsArray} renderItem={renderIngredient} keyExtractor={(item) => `${item.recipeId}`} />
+    </View>
+  );
+}
