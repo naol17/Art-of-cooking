a6a4d39eefc5e3031a1655c3654d4f54d67ab862
Naol Dame <naododa5566@gmail.com>
2022-05-27 12:41:53 +0300

    Define search area

---
+++ my-recipe/src/screens/Search/SearchScreen.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useLayoutEffect, useState } from "react";
+import { FlatList, Text, View, Image, TouchableHighlight, Pressable } from "react-native";
+import styles from "./styles";
+import MenuImage from "../../components/MenuImage/MenuImage";
+import { getCategoryName, getRecipesByRecipeName, getRecipesByCategoryName, getRecipesByIngredientName } from "../../data/MockDataAPI";
+import { TextInput } from "react-native-gesture-handler";
+
+export default function SearchScreen(props) {
+  const { navigation } = props;
+
+  const [value, setValue] = useState("");
+  const [data, setData] = useState([]);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <MenuImage
+          onPress={() => {
+            navigation.openDrawer();
+          }}
+        />
+      ),
+      headerTitle: () => (
+        <View style={styles.searchContainer}>
+          <Image style={styles.searchIcon} source={require("../../../assets/icons/search.png")} />
+          <TextInput
+            style={styles.searchInput}
+            onChangeText={handleSearch}
+            value={value}
+          />
+          <Pressable onPress={() => handleSearch("")}>
+          <Image style={styles.searchIcon} source={require("../../../assets/icons/close.png")} />
+          </Pressable>
+        </View>
+      ),
+      headerRight: () => <View />,
+    });
+  }, [value]);
+
+  useEffect(() => {}, [value]);
