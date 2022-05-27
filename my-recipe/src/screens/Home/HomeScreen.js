9901983a90607b52f868f52230b626fb5a9af1ae
Naol Dame <naododa5566@gmail.com>
2022-05-27 11:58:18 +0300

    rendering the recipe

--- my-recipe/src/screens/Home/HomeScreen.js
+++ my-recipe/src/screens/Home/HomeScreen.js
@@ -23,3 +23,19 @@ export default function HomeScreen(props) {
   const onPressRecipe = (item) => {
     navigation.navigate("Recipe", { item });
   };
+  const renderRecipes = ({ item }) => (
+    <TouchableHighlight underlayColor="rgba(73,182,77,0.9)" onPress={() => onPressRecipe(item)}>
+      <View style={styles.container}>
+        <Image style={styles.photo} source={{ uri: item.photo_url }} />
+        <Text style={styles.title}>{item.title}</Text>
+        <Text style={styles.category}>{getCategoryName(item.categoryId)}</Text>
+      </View>
+    </TouchableHighlight>
+  );
+
+  return (
+    <View>
+      <FlatList vertical showsVerticalScrollIndicator={false} numColumns={2} data={recipes} renderItem={renderRecipes} keyExtractor={(item) => `${item.recipeId}`} />
+    </View>
+  );
+}
