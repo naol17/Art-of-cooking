c47884abae2c4c1e5122906083ad72d1a1f35c24
Naol Dame <naododa5566@gmail.com>
2022-05-27 12:15:56 +0300

    Style sheet for ingredient detail page

---
+++ my-recipe/src/screens/IngredientsDetail/style.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Dimensions } from 'react-native';
+// screen sizing
+const { width, height } = Dimensions.get('window');
+// orientation must fixed
+const SCREEN_WIDTH = width < height ? width : height;
+
+const numColumns = 3;
+// item size
+const RECIPE_ITEM_HEIGHT = 100;
+const RECIPE_ITEM_OFFSET = 10;
+const RECIPE_ITEM_MARGIN = RECIPE_ITEM_OFFSET * 2;
