1973358ab8286c2e81926a9daf6c63781686875f
Naol Dame <naododa5566@gmail.com>
2022-05-27 11:57:05 +0300

    Home screen

---
+++ my-recipe/src/screens/Home/HomeScreen.js
@@ -0,0 +1,9 @@
+import React, { useLayoutEffect } from "react";
+import { FlatList, Text, View, TouchableHighlight, Image } from "react-native";
+import styles from "./styles";
+import { recipes } from "../../data/dataArrays";
+import MenuImage from "../../components/MenuImage/MenuImage";
+import { getCategoryName } from "../../data/MockDataAPI";
+
+export default function HomeScreen(props) {
+  const { navigation } = props;
