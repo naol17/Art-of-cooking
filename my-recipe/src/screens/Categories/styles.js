6b969fa1113bf754d4a092b90d7012e8f4f7af03
Naol Dame <naododa5566@gmail.com>
2022-05-27 11:49:32 +0300

    styling category page

---
+++ my-recipe/src/screens/Categories/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  categoriesItemContainer: {
+    flex: 1,
+    margin: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 215,
+    borderColor: '#cccccc',
+    borderWidth: 0.5,
+    borderRadius: 20,
+  },
